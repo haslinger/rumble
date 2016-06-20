56fc5e463bae23ec1922f194cd5e85e827d48246
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-06-20 14:27:31 +0200

    create users

--- web/views/error_helpers.ex
+++ web/views/error_helpers.ex
@@ -10,7 +10,7 @@ defmodule Rumbl.ErrorHelpers do
   """
   def error_tag(form, field) do
     if error = form.errors[field] do
-      content_tag :span, translate_error(error), class: "help-block"
+      content_tag :span, translate_error(error), class: "text-danger"
     end
   end
 

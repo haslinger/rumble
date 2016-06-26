ec6b9b9936abd8399fb7e85cd46f78ed33c28683
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-06-26 18:46:49 +0200

    tests passing again

--- apps/rumble/config/dev.secrets.exs
+++
@@ -1,3 +0,0 @@
-use Mix.Config
-
-config :info_sys, :wolfram, app_id: "7VLQT9-669A5V5G58"
---
+++ config/prod.secret.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :rumbl, Rumbl.Endpoint,
+  secret_key_base: "855zT5UTWyFqD+ojN4PIylAnzURAknGToTh539KQd30TGM/vLejePO4jiIW9okev"
+
+# Configure your database
+config :rumbl, Rumbl.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "rumbl_prod",
+  pool_size: 20
